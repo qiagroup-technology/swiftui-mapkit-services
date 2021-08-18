de1bafca4c21bae6d677756cb5add22d65036a74
webmaster-qiagroup <webmaster@qiagroup.com.au>
2021-08-18 13:10:41 +1000

    endpoint fix 6
    endpoint fix 6

--- Sources/SwiftUIMapKitServices/Networking/Endpoint.swift
+++ Sources/SwiftUIMapKitServices/Networking/Endpoint.swift
@@ -199,9 +199,9 @@ extension Endpoint where A: Decodable {
             return Result {
                 guard let dat = data else { throw NoDataError() }
                 ///return try decoder.decode(A.self, from: dat)
-                var decodedResponse = ""
+                
                 do {
-                      decodedResponse = try decoder.decode(A.self, from: dat)
+                      let decodedResponse = try decoder.decode(A.self, from: dat)
                     } catch DecodingError.keyNotFound(let key, let context) {
                        Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
                    } catch DecodingError.valueNotFound(let type, let context) {
@@ -213,7 +213,7 @@ extension Endpoint where A: Decodable {
                    } catch let error as NSError {
                        NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
                    }
-                   return decodedResponse
+                   return try decoder.decode(A.self, from: dat)
             }
         }
     }
