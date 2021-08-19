3ea745d10a8c69b688b6b2847976ecca0e3023f8
webmaster-qiagroup <webmaster@qiagroup.com.au>
2021-08-19 13:51:44 +1000

    fixed new inspectors api
    fixed new inspector api

--- Sources/SwiftUIMapKitServices/API/API.swift
+++ Sources/SwiftUIMapKitServices/API/API.swift
@@ -12,16 +12,17 @@ public struct API {
     // MARK: - Instance Properties
 
     private var baseURL: URL
-    private var selectedPath: String
 
     public var jobs: Endpoint<[JobResponse]> {
-        return Endpoint(json: .get, url: baseURL.appendingPathComponent(selectedPath), decoder: decoder)
+        return Endpoint(json: .get, url: baseURL.appendingPathComponent("jobs), decoder: decoder)
+    }
+    public var inspectors: Endpoint<[InspectorResponse]> {
+        return Endpoint(json: .get, url: baseURL.appendingPathComponent("inspectors"), decoder: decoder)
     }
 
     // MARK: - Initialization
 
-    public init(baseURL: URL, selectedPath: String) {
+    public init(baseURL: URL) {
         self.baseURL = baseURL
-        self.selectedPath = selectedPath
     }
 }
